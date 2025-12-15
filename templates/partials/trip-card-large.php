68953f0a8d64ad617ce6b8d18e8ae92917785c66
Augusto A. Silva <augusto@solis.com.br>
2025-12-15 09:46:58 -0300

    Ajustando

--- templates/partials/trip-card-large.php
+++ templates/partials/trip-card-large.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Partial para card grande (slider destaque 1)
  *
@@ -7,19 +8,19 @@
  * @var WTE_Sliders_Query $query Instância da classe de query
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 ?>
 
 <div class="wte-trip-card-large">
     <div class="wte-trip-media">
-        <?php if ( ! empty( $trip->video ) ) : ?>
-            <?php $embed_url = $query->get_video_embed_url( $trip->video ); ?>
-            <?php if ( $embed_url ) : ?>
+        <?php if (! empty($trip->video)) : ?>
+            <?php $embed_url = $query->get_video_embed_url($trip->video); ?>
+            <?php if ($embed_url) : ?>
                 <div class="wte-trip-video">
                     <iframe
-                        src="<?php echo esc_url( $embed_url ); ?>"
+                        src="<?php echo esc_url($embed_url); ?>"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen
@@ -27,13 +28,13 @@ if ( ! defined( 'ABSPATH' ) ) {
                     </iframe>
                 </div>
             <?php endif; ?>
-        <?php elseif ( ! empty( $trip->image ) ) : ?>
+        <?php elseif (! empty($trip->image)) : ?>
             <div class="wte-trip-image">
-                <img src="<?php echo esc_url( $trip->image ); ?>" alt="<?php echo esc_attr( $trip->title ); ?>" loading="lazy">
+                <img src="<?php echo esc_url($trip->image); ?>" alt="<?php echo esc_attr($trip->title); ?>" loading="lazy">
                 <div class="wte-trip-play-overlay">
                     <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="40" cy="40" r="40" fill="white" opacity="0.9"/>
-                        <path d="M32 25L55 40L32 55V25Z" fill="#00BCD4"/>
+                        <circle cx="40" cy="40" r="40" fill="white" opacity="0.9" />
+                        <path d="M32 25L55 40L32 55V25Z" fill="#00BCD4" />
                     </svg>
                 </div>
             </div>
@@ -42,30 +43,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <div class="wte-trip-content">
         <h3 class="wte-trip-title">
-            <a href="<?php echo esc_url( $trip->permalink ); ?>">
-                <?php echo esc_html( $trip->title ); ?>
+            <a href="<?php echo esc_url($trip->permalink); ?>">
+                <?php echo esc_html($trip->title); ?>
             </a>
         </h3>
 
         <div class="wte-trip-meta">
-            <?php if ( ! empty( $trip->duration ) ) : ?>
+            <?php if (! empty($trip->duration)) : ?>
                 <div class="wte-trip-duration">
-                    <img src="<?php echo esc_url( $loader->get_asset_url( 'images/icon-clock.svg' ) ); ?>" alt="" width="16" height="16">
-                    <span><?php echo esc_html( $trip->duration ); ?></span>
+                    <img src="<?php echo esc_url($loader->get_asset_url('images/icon-clock-type1.svg')); ?>" alt="" width="16" height="16">
+                    <span><?php echo esc_html($trip->duration); ?></span>
                 </div>
             <?php endif; ?>
 
-            <?php if ( ! empty( $trip->destination ) ) : ?>
+            <?php if (! empty($trip->destination)) : ?>
                 <div class="wte-trip-location">
-                    <img src="<?php echo esc_url( $loader->get_asset_url( 'images/icon-location.svg' ) ); ?>" alt="" width="16" height="16">
-                    <span><?php echo esc_html( $trip->destination ); ?></span>
+                    <img src="<?php echo esc_url($loader->get_asset_url('images/icon-location-type1.svg')); ?>" alt="" width="16" height="16">
+                    <span><?php echo esc_html($trip->destination); ?></span>
                 </div>
             <?php endif; ?>
         </div>
 
         <div class="wte-trip-actions">
-            <a href="<?php echo esc_url( $trip->permalink ); ?>" class="wte-trip-button">
-                <?php esc_html_e( 'Saiba mais', 'wte-sliders' ); ?>
+            <a href="<?php echo esc_url($trip->permalink); ?>" class="wte-trip-button">
+                <?php esc_html_e('Saiba mais', 'wte-sliders'); ?>
             </a>
         </div>
     </div>
